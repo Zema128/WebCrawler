15ec9427f73eb04eff1c18c8bd02778b34958e1c
Zema128 <vladzemec@icloud.com>
2021-12-08 16:46:52 +0300

    First

---
+++ src/main/java/test/webcrawler/ze/ZeApplication.java
@@ -0,0 +1,13 @@
+package test.webcrawler.ze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeApplication.class, args);
+	}
+
+}
