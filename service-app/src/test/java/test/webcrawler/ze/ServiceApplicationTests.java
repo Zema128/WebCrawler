d2078ddb40e99582284a48d6545f1d35fec289b5
Zema128 <vladzemec@icloud.com>
2021-12-09 11:15:04 +0300

    structure refactor

---
+++ service-app/src/test/java/test/webcrawler/ze/ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package test.webcrawler.ze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
